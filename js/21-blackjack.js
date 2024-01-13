3fbc9682b458efce8d3b4b03f6b05a06ce84fca9
Diana Clabel <dianaclabel08@gmail.com>
2024-01-13 00:59:48 -0500

    Se termina ejercicio 21

--- js/21-blackjack.js
+++ js/21-blackjack.js
@@ -1,5 +1,17 @@
 function blackjack(a, b) {
-  if (a === 21) {
+  // Si ambos tienen valor 21 retorna 21
+  if (a === 21 || b === 21) {
     return 21;
+    // si los valores es mayor a 21 retornara 0
+  } else if (a > 21 && b > 21) {
+    return 0;
+    // retornar el numero mas pequeño si tenermos numeros mayores a  21
+  } else if (a <= 21 && b > 21) {
+    return a;
+  } else if (a > 21 && b <= 21) {
+    return b;
+    // si los numeros son menore a 21 retornar el numero mayor
+  } else if (a <= 21 && b <= 21) {
+    return Math.max(a, b);
   }
 }
